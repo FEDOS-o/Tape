e6dd9388a5b851f786905afe08a87fe300484d19
Fedor Shurcov <fedorshurcov1234@mail.ru>
2023-01-04 13:37:54 +0300

    move move_to_start and swap test for tape

--- tests/test.cpp
+++ tests/test.cpp
@@ -155,3 +155,70 @@ TEST(tape, haotic_writing) {
     }
     EXPECT_TRUE(compare_files(filename, cfg.FILE_DIR + "\\" + "haotic_writing_result_formatted"));
 }
+
+TEST(tape, move_to_start) {
+    std::string filename;
+    {
+        Tape t(cfg.FILE_DIR, "sample123");
+        filename = t.filename;
+        for (int i = 1; i < 4; i++) {
+            t.move_right();
+            t.w_current_cell(999 + i);
+        }
+        t.move_to_start();
+        for (int i = 10; i >0; i--) {
+            t.w_current_cell(i);
+            t.move_right();
+        }
+    }
+    EXPECT_TRUE(compare_files(filename, cfg.FILE_DIR + "\\sample321_formatted"));
+}
+
+TEST(tape, move) {
+    std::string filename1, filename2;
+    {
+        Tape t(cfg.FILE_DIR, "sample123");
+        filename1 = t.filename;
+        Tape t_copy = t.make_copy(cfg.TMP_DIR + "\\" + "sample123_copy");
+        filename2 = t_copy.filename;
+        t.move(5);
+        t_copy.move(5);
+        EXPECT_EQ(t.r_current_cell(), t_copy.r_current_cell());
+        t.move(-3);
+        t_copy.move(-3);
+        EXPECT_EQ(t.r_current_cell(), t_copy.r_current_cell());
+        t.move(7);
+        t_copy.move(7);
+        EXPECT_EQ(t.r_current_cell(), t_copy.r_current_cell());
+        t.move_left();
+        t_copy.move(-1);
+        EXPECT_EQ(t.r_current_cell(), t_copy.r_current_cell());
+        t.move_right();
+        t_copy.move(1);
+        EXPECT_EQ(t.r_current_cell(), t_copy.r_current_cell());
+    }
+    EXPECT_TRUE(compare_files(filename1, filename2));
+}
+
+TEST(tape, swap) {
+    std::string filename1, filename2;
+    {
+        Tape t1(cfg.FILE_DIR, "sample123");
+        filename1 = t1.filename;
+        Tape t2(cfg.FILE_DIR, "sample321");
+        filename2 = t2.filename;
+        t1.move(3);
+        t2.move(6);
+        EXPECT_EQ(t1.r_current_cell(),3);
+        EXPECT_EQ(t2.r_current_cell(),5);
+        t1.swap(t2);
+        EXPECT_EQ(t1.r_current_cell(),5);
+        EXPECT_EQ(t2.r_current_cell(),3);
+        t1.move_right();
+        EXPECT_EQ(t1.r_current_cell(),4);
+        t2.move_left();
+        EXPECT_EQ(t2.r_current_cell(),2);
+    }
+    EXPECT_TRUE(compare_files(filename2, cfg.FILE_DIR + "\\sample321_formatted"));
+    EXPECT_TRUE(compare_files(filename1, cfg.FILE_DIR + "\\sample123_formatted"));
+}
