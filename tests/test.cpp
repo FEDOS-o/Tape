d794cd7e2a35a52ff38235e5efdd8268b420310a
Fedor Shurcov <fedorshurcov1234@mail.ru>
2023-01-04 17:25:46 +0300

    more test for test_sorter

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,13 @@
-#include <gtest/gtest.h>
 #include "tape_sorter.h"
+#include <chrono>
+#include <gtest/gtest.h>
+#include <random>
+
+
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
 
 Config cfg("..\\tests\\config.txt");
+
 
 bool compare_files(std::string filename1, std::string filename2) {
     std::ifstream file1(filename1), file2(filename2);
@@ -20,11 +26,11 @@ bool compare_files(std::string filename1, std::string filename2) {
 }
 
 TEST(tape, straight_read) {
-   Tape t(cfg.FILE_DIR, "sample123");
-   for (int i = 1; i < 11; i++) {
-       t.move_right();
-       EXPECT_EQ(t.read(), i);
-   }
+    Tape t(cfg.FILE_DIR, "sample123");
+    for (int i = 1; i < 11; i++) {
+        t.move_right();
+        EXPECT_EQ(t.read(), i);
+    }
 }
 
 TEST(tape, double_read) {
@@ -166,7 +172,7 @@ TEST(tape, move_to_start) {
             t.write(999 + i);
         }
         t.move_to_start();
-        for (int i = 10; i >0; i--) {
+        for (int i = 10; i > 0; i--) {
             t.write(i);
             t.move_right();
         }
@@ -209,15 +215,15 @@ TEST(tape, swap) {
         filename2 = t2.filename;
         t1.move(3);
         t2.move(6);
-        EXPECT_EQ(t1.read(),3);
-        EXPECT_EQ(t2.read(),5);
+        EXPECT_EQ(t1.read(), 3);
+        EXPECT_EQ(t2.read(), 5);
         t1.swap(t2);
-        EXPECT_EQ(t1.read(),5);
-        EXPECT_EQ(t2.read(),3);
+        EXPECT_EQ(t1.read(), 5);
+        EXPECT_EQ(t2.read(), 3);
         t1.move_right();
-        EXPECT_EQ(t1.read(),4);
+        EXPECT_EQ(t1.read(), 4);
         t2.move_left();
-        EXPECT_EQ(t2.read(),2);
+        EXPECT_EQ(t2.read(), 2);
     }
     EXPECT_TRUE(compare_files(filename2, cfg.FILE_DIR + "\\sample321_formatted"));
     EXPECT_TRUE(compare_files(filename1, cfg.FILE_DIR + "\\sample123_formatted"));
@@ -253,5 +259,58 @@ TEST(tape_sorter, different_M) {
         }
         bool res = compare_files(cfg.FILE_DIR + "\\sample123_formatted", cfg.FILE_DIR + "\\sample123_sort_result");
         EXPECT_TRUE(res);
+    }
+}
+
+TEST(tape_sorter, random_numbers) {
+    cfg.N = 25;
+    cfg.M = 7;
+    std::vector<int> result;
+    std::ofstream random_test(cfg.FILE_DIR + "\\random_test");
+    for (int i = 0; i < cfg.N; i++) {
+        int r = static_cast<int>(rnd());
+        result.push_back(r);
+        random_test << r << ' ';
+    }
+    random_test.close();
+    std::sort(result.begin(), result.end());
+    random_test = std::ofstream(cfg.FILE_DIR + "\\random_test_sorted");
+    for (int i = 0; i < cfg.N; i++) {
+        random_test << result[i] << ' ';
+    }
+    random_test.close();
+    {
+        Tape answer(cfg.FILE_DIR, "random_test_sorted");
+        Tape_sorter::sort("random_test", "random_test_result");
+    }
+    bool cmp = compare_files(cfg.TMP_DIR + "\\random_test_sorted_formatted", cfg.FILE_DIR + "\\random_test_result");
+    EXPECT_TRUE(cmp);
+}
+
+TEST(tape_sorter, random_number_diff_M) {
+    cfg.N = 40;
+
+    for (int j = 2; j < 50; j++) {
+        cfg.M = j;
+        std::vector<int> result;
+        std::ofstream random_test(cfg.FILE_DIR + "\\random_test");
+        for (int i = 0; i < cfg.N; i++) {
+            int r = static_cast<int>(rnd());
+            result.push_back(r);
+            random_test << r << ' ';
+        }
+        random_test.close();
+        std::sort(result.begin(), result.end());
+        random_test = std::ofstream(cfg.FILE_DIR + "\\random_test_sorted");
+        for (int i = 0; i < cfg.N; i++) {
+            random_test << result[i] << ' ';
+        }
+        random_test.close();
+        {
+            Tape answer(cfg.FILE_DIR, "random_test_sorted");
+            Tape_sorter::sort("random_test", "random_test_result");
+        }
+        bool cmp = compare_files(cfg.TMP_DIR + "\\random_test_sorted_formatted", cfg.FILE_DIR + "\\random_test_result");
+        EXPECT_TRUE(cmp);
     }
 }
